43f85928704c1b30f5ba2fb0ecbc8a52ac51e035
Abhinav Singh <abhiabhiabhiabhinav@gmail.com>
2016-10-20 15:35:01 +0530

    Added plane camera script

---
+++ Assets/Scripts/PlaneCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform enemyTarget;
+    [SerializeField]
+    private Transform playerTarget;
+    [SerializeField]
+    private float zoomFactor;
+    [SerializeField]
+    private float zoomOffset;
+    [SerializeField]
+    private float heightOffset;
+
+    void LateUpdate()
+    {
+        var midPoint = (enemyTarget.position + playerTarget.position) / 2;
+        var lineVector = enemyTarget.position - playerTarget.position;
+        float distance = lineVector.magnitude;
+        var perpendicularToLine = Vector3.Cross(lineVector, Vector3.up);
+        perpendicularToLine = perpendicularToLine * zoomFactor;
+        if (perpendicularToLine.magnitude < zoomOffset)
+        {
+            perpendicularToLine.Normalize();
+            perpendicularToLine = perpendicularToLine * zoomOffset;
+        }
+        var newPosition = midPoint - perpendicularToLine;
+        newPosition.y = newPosition.y + heightOffset;
+        transform.position = newPosition;
+        transform.rotation = Quaternion.LookRotation(perpendicularToLine);
+    }
+}
